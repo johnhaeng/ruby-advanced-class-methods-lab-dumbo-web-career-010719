bc2aef1aec0f9725e54e35837f9a999eca9fc0eb
Jun Haeng Lee <johnhaeng@gmail.com>
2019-01-14 05:45:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,40 @@ class Song
   end
 
   def self.create
-    song = self.New
+    song = self.new
     @@all << song
+    song
   end
 
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+  
   def save
     self.class.all << self
   end
 
+  def self.find_by_name(name)
+    @@all.each do |song|
+      if song.name != name
+        false
+      else
+        return song
+      end 
+    end
+  end 
+  
+  
+   def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
 end
